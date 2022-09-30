b465cc27e52454c9508441a6deaea6897f90dbfb
Gabriel Bordais-Poulard <bordais-@insa-toulouse.fr>
2022-09-30 13:15:46 +0200

    include path

--- Projet1/Source/Principal.c
+++ Projet1/Source/Principal.c
@@ -1,5 +1,8 @@
 #include "stm32f10x.h"
 #include "./../../MesDrivers/Include/Driver_GPIO.h"
+		MyGPIO_Struct_TypeDef LED;
+		MyGPIO_Struct_TypeDef Btn;
+
 
 int main ( void )
 {
@@ -8,14 +11,12 @@ int main ( void )
 	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPBEN | RCC_APB2ENR_IOPCEN;
 
 		//PC10  LED
-		MyGPIO_Struct_TypeDef LED;
 	  LED.GPIO=GPIOC;
 		LED.GPIO_Pin=10;
 		LED.GPIO_Conf=Out_Ppull;
 		MyGPIO_Init(&LED);
 	
 		//PC8 BP
-			MyGPIO_Struct_TypeDef Btn;
 	  Btn.GPIO=GPIOC;
 		Btn.GPIO_Pin=8;
 		Btn.GPIO_Conf=In_Floating;
