b168202f65075f65e0840db2fcfdb92a0db68f71
Valentin Guittard <vguittar@insa-toulouse.fr>
2022-09-30 13:43:46 +0200

    réparation de la libraire (driver) et main + tests validés

--- MesProjetsSTM32/MesDrivers/Source/Driver_GPIO.c
+++ MesProjetsSTM32/MesDrivers/Source/Driver_GPIO.c
@@ -1,26 +1,32 @@
 #include <Driver_GPIO.h>
+#include <string.h>
 
 void MyGPIO_Init ( MyGPIO_Struct_TypeDef * GPIOStructPtr ){
-	RCC->APB2ENR |=(0x01 << 2)|(0x01 << 3)|(0x01 << 4); //init de la clock du GPIO
+	if (GPIOStructPtr->GPIO_Pin <8){
+		GPIOStructPtr->GPIO->CRL &= ~(0xF << ((GPIOStructPtr->GPIO_Pin)*4-1));
+		GPIOStructPtr->GPIO->CRL |= (GPIOStructPtr->GPIO_Conf << ((GPIOStructPtr->GPIO_Pin)*4-1));
+	}
+	else{
+		GPIOStructPtr->GPIO->CRH &= ~(0xF << ((GPIOStructPtr->GPIO_Pin-8)*4-1));
+		GPIOStructPtr->GPIO->CRH |= (GPIOStructPtr->GPIO_Conf << ((GPIOStructPtr->GPIO_Pin-8)*4-1));
+	}
 }
 int MyGPIO_Read ( GPIO_TypeDef * GPIO , char GPIO_Pin ){
-	
+	uint32_t masque = (0xFFFFFFFF);
+	masque &= ~(0x1 << (GPIO_Pin));
+	return (GPIO->IDR & ~(masque))>0;
 }
 void MyGPIO_Set ( GPIO_TypeDef * GPIO , char GPIO_Pin ){
-
+	GPIO->ODR |= 0x1 << (GPIO_Pin);
 }
 void MyGPIO_Reset ( GPIO_TypeDef * GPIO , char GPIO_Pin ){
-	
+	GPIO->ODR &= ~(0x1 << (GPIO_Pin));
 }
 void MyGPIO_Toggle ( GPIO_TypeDef * GPIO , char GPIO_Pin ){
-	
+	if (MyGPIO_Read(GPIO,GPIO_Pin)==0){
+		MyGPIO_Set(GPIO,GPIO_Pin);
+	}
+	else{
+		MyGPIO_Reset(GPIO,GPIO_Pin);
+	}
 }
-
-int main (void){
-	while(1);
-}
-
-// si pin > 7 => taper dans CRH, sinon taper dans CRL
-// Dans CRH : GPIO_Conf << ((PIN-7)*4)-1 || Dans CRL : GPIO_Conf  << ((PIN+1)*4)-1
-
-//Read / Set / Reset / Toggle -> modif bit correspondant à la broche (ou read)
--- MesProjetsSTM32/Projet_1/Source/main.c
+++ MesProjetsSTM32/Projet_1/Source/main.c
@@ -1,17 +1,29 @@
 #include "stm32f10x.h"
 #include "Driver_GPIO.h"
-
+	
 int main (void)
 {
+	MyGPIO_Struct_TypeDef GPIOC8 = {GPIOC, 8, In_Floating};
+	MyGPIO_Struct_TypeDef GPIOC10 = {GPIOC, 10, Out_Ppull};
 	RCC->APB2ENR |= (0x01 << 2) | (0x01 << 3) | (0x01 << 4) ; //allumage des clocks de A B et C
-	GPIOC->CRH |= (0x01 << 9) | (0x01 << 2) | (0x01 << 10) ; // passer le bit 10 à 0 pour revenir en push-pull
-	GPIOC->CRH &= ~((0x01 << 11) | (0x01 << 8) | (0x01 << 0) | (0x01 << 1) | (0x01 << 3)) ;
+	// GPIOC->CRH |= (0x01 << 9) | (0x01 << 2) | (0x01 << 10) ; // passer le bit 10 à 0 pour revenir en push-pull
+	// GPIOC->CRH &= ~((0x01 << 11) | (0x01 << 8) | (0x01 << 0) | (0x01 << 1) | (0x01 << 3)) ;
+	MyGPIO_Init(&GPIOC8);
+	MyGPIO_Init(&GPIOC10);
+	
 	do
 	{
+		/*
 		if ((GPIOC->IDR & GPIO_IDR_IDR8) != 0) {
 			GPIOC->ODR &= ~(GPIO_ODR_ODR10) ;
 		} else {
 			GPIOC->ODR |= GPIO_ODR_ODR10 ;
+		}
+		*/
+		if (MyGPIO_Read(GPIOC8.GPIO, 8)) {
+			MyGPIO_Set(GPIOC10.GPIO, 10) ;
+		} else {
+			MyGPIO_Reset(GPIOC10.GPIO, 10) ;
 		}
 	} while ( 1 ) ;
 }
